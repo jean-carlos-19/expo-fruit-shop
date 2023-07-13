9860adca7eb6320a93b10b5949b9109fe951b76c
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-13 10:38:35 -0500

    Modificado FruitCardSales.tsx: Construccion completada del componente

--- atomic/component/FruitCardSales.tsx
+++ atomic/component/FruitCardSales.tsx
@@ -1,10 +1,44 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 
-const FruitCardSales = () => {
+interface fruit_card_sales{
+    fruit:{
+        color:any,
+        desc:string,
+        image:number,
+        name:string,
+        price:string,
+        shadow:string,
+        stars:number
+    }
+}
+const FruitCardSales = (props:fruit_card_sales) => {
+  const {fruit} = props;
+ const {color,desc,image,name,price,shadow, stars} = fruit;
   return (
-    <View>
-      
+    <View className='mr-6'>
+     <TouchableOpacity
+        className='flex-row justify-center shadow-lg z-20'
+     >
+        <Image 
+            source={image}
+            style={{
+                height:65, width:65,
+                shadowColor:shadow,
+                overflow:"visible",
+                shadowRadius:15,
+                shadowOffset:{width:0,height:20},
+                shadowOpacity:.4,
+            }}
+        />
+     </TouchableOpacity>
+     <View 
+        style={{backgroundColor:color(0.4), height:75, width:80}}
+        className="rounded-3xl flex justify-end items-center -mt-10">
+        <Text className="font-semibold text-center text-gray-800 pb-3 tracking-wide">
+            $ {price}
+        </Text>
+     </View>
     </View>
   )
 }
